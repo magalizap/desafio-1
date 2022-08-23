d999704eff1a85a5e01f9205f1d7ededa50f5987
Magali Zapata <magalisol2307@gmail.com>
2022-08-23 12:15:54 -0300

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import personas from './data/personas.json'
 
-function App() {
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Mi app en React: personajes de Friends!</h1>
+      {
+        personas.map(persona => (
+          <div className="box">
+            <p>Nombre: {persona.nombre}</p>
+            <p>Apellido: {persona.apellido}</p>
+            <p>Edad: {persona.edad}</p>
+
+            <hr />
+          </div>
+        ))
+      }
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
